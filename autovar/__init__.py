62078a14ef4082778b756e5adf733baa714d3877
Joe Singleton <joesingo@gmail.com>
2019-07-15 12:38:48 +0100

    Import * from top-level __init__.py
    This means external code can import all functionality without
    __init__.py being updated after each change to autovar

--- autovar/__init__.py
+++ autovar/__init__.py
@@ -1,6 +1,8 @@
-from autovar.analyse import calculate_curves, photometric_calculations
-from autovar.comparison import find_comparisons
-from autovar.eebls import plot_bls
-from autovar.identify import find_stars
-from autovar.plots import make_plots, calibrated_plots
-from autovar.utils import get_targets, folder_setup, AutovarException, cleanup
+from autovar.analyse import *
+from autovar.comparison import *
+from autovar.detrend import *
+from autovar.eebls import *
+from autovar.identify import *
+from autovar.main import *
+from autovar.plots import *
+from autovar.utils import *
